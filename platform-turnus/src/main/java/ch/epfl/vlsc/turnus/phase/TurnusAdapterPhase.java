220a47399e0969baab1d17e8e3b295ee10cec327
endrix <endri.bezati@gmail.com>
2020-06-11 23:18:19 +0200

    (platform-turnus) Adding setting etracez-path for the compressed execution trace generated in runtime by code generator. Tested with an etracez from turnus.orcc.profiler. Generated TURNUS trace on the target path.

--- platform-turnus/src/main/java/ch/epfl/vlsc/turnus/phase/TurnusAdapterPhase.java
+++ platform-turnus/src/main/java/ch/epfl/vlsc/turnus/phase/TurnusAdapterPhase.java
@@ -3,9 +3,12 @@ package ch.epfl.vlsc.turnus.phase;
 import ch.epfl.vlsc.turnus.adapter.TurnusModelAdapter;
 import org.apache.log4j.BasicConfigurator;
 import se.lth.cs.tycho.compiler.CompilationTask;
+import se.lth.cs.tycho.compiler.Compiler;
 import se.lth.cs.tycho.compiler.Context;
+import se.lth.cs.tycho.ir.util.ImmutableList;
 import se.lth.cs.tycho.phase.Phase;
 import se.lth.cs.tycho.reporting.CompilationException;
+import se.lth.cs.tycho.settings.PathSetting;
 import se.lth.cs.tycho.settings.Setting;
 import turnus.analysis.profiler.dynamic.DynamicProfiler;
 import turnus.analysis.profiler.dynamic.util.ProfiledExecutionDataReader;
@@ -18,13 +21,32 @@ import turnus.model.versioning.Versioner;
 import turnus.model.versioning.impl.GitVersioner;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static turnus.common.TurnusConstants.DEFAULT_VERSIONER;
-import static turnus.common.TurnusOptions.CAL_PROJECT;
-import static turnus.common.TurnusOptions.VERSIONER;
+import static turnus.common.TurnusOptions.*;
 
 public class TurnusAdapterPhase implements Phase {
+
+    public static final Setting<Path> etracezPath = new PathSetting() {
+        @Override
+        public String getKey() {
+            return "etracez-path";
+        }
+
+        @Override
+        public String getDescription() {
+            return "Compressed execution trace json file.";
+        }
+
+        @Override
+        public Path defaultValue(se.lth.cs.tycho.settings.Configuration configuration) {
+            return Paths.get("");
+        }
+    };
+
     @Override
     public String getDescription() {
         return "StreamBlocks Turnus Adapter Phase";
@@ -38,15 +60,29 @@ public class TurnusAdapterPhase implements Phase {
         BasicConfigurator.configure();
 
         Logger.info("Configuring the project");
+        Path targetPath = context.getConfiguration().get(Compiler.targetPath);
+        File directory = targetPath.toFile();
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
 
         Configuration configuration = new Configuration();
         configuration.setValue(CAL_PROJECT, task.getIdentifier().getLast().toString());
         configuration.setValue(VERSIONER, DEFAULT_VERSIONER);
+        configuration.setValue(OUTPUT_DIRECTORY, targetPath.toFile());
+        configuration.setValue(COMPRESS_TRACE, false);
+        configuration.setValue(EXPORT_GANTT_CHART, false);
+        configuration.setValue(EXPORT_TRACE, true);
 
 
         Logger.info("* QID: %s", task.getIdentifier().toString());
+        Logger.info("* Target Path: %s", targetPath.toAbsolutePath().toString());
 
-        String profiledExecutionFile = "";
+        Path etracezPath = context.getConfiguration().get(TurnusAdapterPhase.etracezPath);
+        if (!etracezPath.toFile().exists()) {
+            Logger.error("Execution trace file does not exist: " + etracezPath.toAbsolutePath().toString());
+            return null;
+        }
 
         try {
             Versioner versioner = new GitVersioner();
@@ -59,22 +95,25 @@ public class TurnusAdapterPhase implements Phase {
             DynamicProfiler profiler = new DynamicProfiler(network);
             profiler.setConfiguration(configuration);
 
-            File jsonFile = new File(profiledExecutionFile);
+            Logger.info("Parsing the execution profiling data");
+            File jsonFile = etracezPath.toFile();
             ProfiledExecutionDataReader reader = new ProfiledExecutionDataReader(profiler, jsonFile);
             reader.start();
             reader.join();
 
+            Logger.info("===== ANALYSIS DONE ====");
         } catch (TurnusException e) {
             e.printStackTrace();
         }
 
-        Logger.info("Parsing the execution profiling data");
 
         return null;
     }
 
     @Override
     public List<Setting<?>> getPhaseSettings() {
-        return null;
+        return ImmutableList.of(etracezPath);
     }
+
+
 }
